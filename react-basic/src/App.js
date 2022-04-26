a9719c40564d32548d72c7dc7f2a07a381abcdb5
phuthinan <s.phuthinan@gmail.com>
2022-04-27 00:10:09 +0700

    provider consumer

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,7 +2,7 @@ import Transaction from "./component/Transaction";
 import './App.css'
 import FormComponent from "./component/FormComponent";
 import { useState } from "react";
-
+import DataContext from "./data/DataContext.js";
 function App() {
   const design = {color:'red',textAlign:"center",fontSize:"1.5rem"}
   const initData=[]
@@ -13,11 +13,13 @@ function App() {
     })
   }
   return (
-    <div className="container">
-      <h1 style={design}>บัญชี รายรับ รายจ่าย</h1>
-      <FormComponent onAddItem={onAddNewItem}/>
-      <Transaction items = {items}/>
-    </div>
+     <DataContext.Provider value={"Hey"}>
+       <div className="container">
+        <h1 style={design}>บัญชี รายรับ รายจ่าย</h1>
+        <FormComponent onAddItem={onAddNewItem}/>
+        <Transaction items = {items}/>
+      </div>
+     </DataContext.Provider>
     );
 }
 export default App;
--- react-basic/src/component/Transaction.js
+++ react-basic/src/component/Transaction.js
@@ -1,16 +1,21 @@
 import Item from "./Item";
 import './Transaction.css'
-
+import DataContext from "../data/DataContext";
 
 const Transaction = (props) => {
     const {items} =props
     return (
-      <ul className="item-list">
+      <div>
+        <ul className="item-list">
         {items.map((element)=>{
           // return <Item title={element.title} amount={element.amount}/>
           return <Item {...element} key={element.id}/>
         })}
       </ul>
+      <DataContext.Consumer>
+        {value=><p>{value}</p>}
+      </DataContext.Consumer>
+      </div>
     );
   }
   export default Transaction;
