6c1bc01623a795819898bedb54d685e841ba8d0d
phuthinan <s.phuthinan@gmail.com>
2022-04-27 00:24:04 +0700

    provider consumer

--- react-basic/src/component/Transaction.js
+++ react-basic/src/component/Transaction.js
@@ -1,6 +1,6 @@
 import Item from "./Item";
 import './Transaction.css'
-import DataContext from "../data/DataContext";
+import DataContext from "../data/DataContext.js";
 
 const Transaction = (props) => {
     const {items} =props
